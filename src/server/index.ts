469466270454eb28c796eba4d27a26f2f6a95488
somenerd_bootdev <somenerdbootdev@gmail.com>
2026-05-04 11:36:45 -0400

    Initial connection test

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,14 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Successfully connected to RabbitMQ");
+  process.on("SIGINT", () => {
+    console.log("Peril is shutting down...")
+    conn.close();
+  });
 }
 
 main().catch((err) => {
