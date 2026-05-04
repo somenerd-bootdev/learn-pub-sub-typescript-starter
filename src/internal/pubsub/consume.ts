91714fa29c42b7eed6df3d1f116764912295f6c2
somenerd_bootdev <somenerdbootdev@gmail.com>
2026-05-04 19:13:21 -0400

    Who knows

---
+++ src/internal/pubsub/consume.ts
@@ -0,0 +1,25 @@
+import amqp, { type Channel } from "amqplib";
+
+export enum SimpleQueueType {
+  Durable,
+  Transient,
+}
+
+export async function declareAndBind(
+  conn: amqp.ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType,
+): Promise<[Channel, amqp.Replies.AssertQueue]> {
+  const ch = await conn.createChannel();
+
+  const queue = await ch.assertQueue(queueName, {
+    durable: queueType === SimpleQueueType.Durable,
+    exclusive: queueType !== SimpleQueueType.Durable,
+    autoDelete: queueType !== SimpleQueueType.Durable,
+  });
+
+  await ch.bindQueue(queue.queue, exchange, key);
+  return [ch, queue];
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,16 @@
 import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import type { PlayingState } from "../internal/gamelogic/gamestate.js";
 
 async function main() {
   console.log("Starting Peril server...");
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
   console.log("Successfully connected to RabbitMQ");
+  const confirmChannel = await conn.createConfirmChannel();
+  const playingState = { isPaused: true };
+  publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, playingState);
   process.on("SIGINT", () => {
     console.log("Peril is shutting down...")
     conn.close();
